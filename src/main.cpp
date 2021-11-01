5d9f73b07f7e30ca5210821915d7de08619696b5
Adrien <adrien.3100@gmail.com>
2021-11-01 23:27:32 +0100

    use adafruit feed

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,12 @@
 #include <Arduino.h>
+#include <AdafruitIO_WiFi.h>
+#include "../config.h"
 
-#include <wifi_con.h>
-#include <config.h>
+// Prototype
+AdafruitIO_WiFi io(IO_USERNAME, IO_KEY, WIFI_SSID, WIFI_PWD);  // Set up the adafruit WiFi client.
+AdafruitIO_Feed *moisture = io.feed(IO_FEED_NAME); // Set up the feed.
 
-
+// Variables 
 int soil_moisture;
 int soil_moisture_percent;
 
@@ -12,18 +15,47 @@ void setup() {
 
   pinMode(MOISTURE_SENSOR_PIN,INPUT);
   adcAttachPin(MOISTURE_SENSOR_PIN);
+  
+  // connect to io.adafruit.com
+  Serial.print("Connecting to Adafruit IO");
+  io.connect();
 
-  wifi_connect(&Serial);
-  //esp_sleep_enable_timer_wakeup(TIME_TO_SECOND * uS_TO_S_FACTOR);
+  // wait for a connection
+  while(io.status() < AIO_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+
+  // we are connected
+  Serial.println();
+  Serial.println(io.statusText());
+  
+  esp_sleep_enable_timer_wakeup(TIME_TO_SECOND * uS_TO_S_FACTOR);  // Timer for the wakeup of the ESP
+  
   delay(2000); 
 }
   
 
 void loop() {
+  // io.run(); is required for all sketches.
+  // it should always be present at the top of your loop
+  // function. it keeps the client connected to
+  // io.adafruit.com, and processes any incoming data.
+  io.run();
+
+  Serial.println("Reading From the Sensor ...");
+
   soil_moisture = analogRead(MOISTURE_SENSOR_PIN);
-  soil_moisture_percent = map(soil_moisture, DRY_VALUE, WET_VALUE, MIN, MAX); 
-  Serial.printf("Moisture : %i%% ", soil_moisture_percent);
-  delay(10000);
- 
-  //esp_deep_sleep_start();
+  soil_moisture_percent = map(soil_moisture, DRY_VALUE, WET_VALUE, MIN, MAX); // calculates the percentage of humidity
+  
+  Serial.printf("Moisture : %i%%\n", soil_moisture_percent);
+  
+  // Send data to the feed.
+  moisture->save(soil_moisture_percent);
+
+  Serial.println("Going to sleep ...\n");
+  
+  delay(2000);
+
+  esp_deep_sleep_start();
 }
